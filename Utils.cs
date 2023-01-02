c2def1beed81d1a867a9263f2eda37ae48eaaac0
Marcelo <marcelo@devitecnologia.com.br>
2023-01-02 20:00:15 -0300

    inicial

---
+++ Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace testenuget;
+
+using System;
+using System.Runtime.InteropServices;
+
+public class Utils
+{
+    public DateTime DataHoraBrasilia()
+    {
+            var s_isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            var s_strBrasil = s_isWindows ? "SA Eastern Standard Time" : "America/Sao_Paulo";
+            return TimeZoneInfo.ConvertTime(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById(s_strBrasil));
+    }
+}
